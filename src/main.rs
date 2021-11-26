169b642b67da6888717581a30952bab351fdc5db
self care <49820562+ICANSTILLHEARYOU@users.noreply.github.com>
2021-11-26 17:36:35 +0300

    -0

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
 use std::io;
 
-fn main() {
+fn code (){
     let mut input = String::new();
     io::stdin().read_line(&mut input);
-
+    // создание двух алфавитов
     let a = String::from("QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnmЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,йцукенгшщзхъфывапролджэячсмитьбю.!@#$%^&_+№:?*();");
     let b = String::from(".!@#$%^&*()+№:?_;ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnmйцукенгшщзхъфывапролджэячсмитьбю");
-        for i in a.len() {
+    //проход по всему алфавиту и замена на второй
+    for i in a.len() {
         input.replace(a[i],b[i]);
     }
     println!{"{}", input};
+}
+
+fn decode(){
+
+}
+
+fn main() {
+    code();
+    decode();
 }
 
 
